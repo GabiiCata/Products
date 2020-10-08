44836317073304a8403c221cfd08bd2347913748
GabiiCata <gabiicata94@gmail.com>
2020-10-08 01:12:01 -0300

    add product

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask (__name__)
 
 from products import products
 
-@app.route('/ping')
+@app.route('/ping' , methods=['GET'])
 def ping():
-    return jsonify(products)
+    return jsonify({"message":"pong"})
+
+@app.route('/products' , methods=['GET'])
+def getProducts():
+    return jsonify( {"products": products} )
+
+@app.route('/products/<string:product_name>')
+def getProduct( product_name ):
+    productsFound = [product for product in products if product['name']== product_name]
+    if (len(productsFound) > 0 ):
+        return jsonify ( {"product" : productsFound[0]} )
+    return jsonify ( {'message' : 'product not found'})
+
+
+@app.route('/products',  methods=['POST'] )
+def addProduct ():
+    new_product = {
+        "name" : request.json["name"],
+        "price": request.json["price"],
+        "quantity": request.json["quantity"]
+    }
+
+    products.append ( new_product )
+    return jsonify ({'message': 'Product added succesfuly',"product" : products})
 
 if __name__ == '__main__':
     app.run(debug=True,port=4000)
