f26d0473c844cca2bb29f29f2aced2f5cad44112
GabiiCata <gabiicata94@gmail.com>
2020-10-07 23:03:43 -0300

    show products json object

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask (__name__)
+
+from products import products
+
+@app.route('/ping')
+def ping():
+    return jsonify(products)
 
 if __name__ == '__main__':
     app.run(debug=True,port=4000)
